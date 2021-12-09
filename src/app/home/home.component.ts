7d069f58109e12701f0f1998dfce875ec5b2a8fc
Marc Bakker <webdev@marcbakker.com>
2021-12-09 14:25:13 +0100

    2: Use RXJS fromEvent operator instead of input event handler

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import * as randomWords from 'random-words';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import * as randomWords from "random-words";
+import { fromEvent, Observable, Subscription } from "rxjs";
 
 export interface IEntry {
   name: string;
@@ -8,36 +15,44 @@ export interface IEntry {
 }
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  @ViewChild("query") query: ElementRef;
+  subscriptions = new Subscription();
 
   data: IEntry[];
   page = 1;
   pageSize = 20;
   emptyArray = new Array(1000);
-
   filteredData: IEntry[];
 
-  constructor() {
-  }
-
   ngOnInit() {
-
     this.data = [];
 
     for (let i = 0; i < 10000; i++) {
       this.data.push({
-        name: randomWords({exactly: 3, join: ' '}),
-        description: randomWords({exactly: 100, join: ' '}),
-        status: ['new', 'submitted', 'failed'][Math.floor(Math.random() * 3)]
+        name: randomWords({ exactly: 3, join: " " }),
+        description: randomWords({ exactly: 100, join: " " }),
+        status: ["new", "submitted", "failed"][Math.floor(Math.random() * 3)],
       });
     }
 
-    this.populateFilteredData('');
+    this.populateFilteredData("");
+  }
 
+  ngAfterViewInit() {
+    this.subscriptions.add(
+      fromEvent(this.query.nativeElement, "input").subscribe(
+        (e: KeyboardEvent) => this.updateFilter(e)
+      )
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 
   populateFilteredData(filter) {
@@ -45,13 +60,16 @@ export class HomeComponent implements OnInit {
       this.filteredData = this.data.slice();
     }
 
-    this.filteredData = this.data.filter(entry => {
-      return entry.name.indexOf(filter) !== -1 || entry.description.indexOf(filter) !== -1 || entry.status.indexOf(filter) !== -1;
+    this.filteredData = this.data.filter((entry) => {
+      return (
+        entry.name.indexOf(filter) !== -1 ||
+        entry.description.indexOf(filter) !== -1 ||
+        entry.status.indexOf(filter) !== -1
+      );
     });
   }
 
   updateFilter($event: KeyboardEvent) {
     this.populateFilteredData(($event.target as HTMLInputElement).value);
   }
-
 }
